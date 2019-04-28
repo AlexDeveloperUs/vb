a47d2f8cb28962bbe6f739ab3ac1686d25be58a5
AlexDeveloperUs <36299711+AlexDeveloperUs@users.noreply.github.com>
2019-04-29 01:45:16 +0300

    Add files via upload

---
+++ WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.AxAxisMediaControl1 = New AxAXISMEDIACONTROLLib.AxAxisMediaControl()
+        CType(Me.AxAxisMediaControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 415)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(126, 415)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(253, 415)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Axis"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(632, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(632, 54)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(632, 99)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(632, 143)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 6
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(632, 184)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 7
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(632, 225)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox6.TabIndex = 8
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(632, 299)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.ReadOnly = True
+        Me.TextBox7.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox7.TabIndex = 9
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(632, 335)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.ReadOnly = True
+        Me.TextBox8.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox8.TabIndex = 10
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(632, 371)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.ReadOnly = True
+        Me.TextBox9.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox9.TabIndex = 11
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(632, 406)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.ReadOnly = True
+        Me.TextBox10.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox10.TabIndex = 12
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(366, 415)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(78, 17)
+        Me.RadioButton1.TabIndex = 13
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Resistance"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(481, 415)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(62, 17)
+        Me.RadioButton2.TabIndex = 14
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Without"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'AxAxisMediaControl1
+        '
+        Me.AxAxisMediaControl1.Enabled = True
+        Me.AxAxisMediaControl1.Location = New System.Drawing.Point(25, 35)
+        Me.AxAxisMediaControl1.Name = "AxAxisMediaControl1"
+        Me.AxAxisMediaControl1.OcxState = CType(resources.GetObject("AxAxisMediaControl1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxAxisMediaControl1.Size = New System.Drawing.Size(517, 319)
+        Me.AxAxisMediaControl1.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.AxAxisMediaControl1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.AxAxisMediaControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents AxAxisMediaControl1 As AxAXISMEDIACONTROLLib.AxAxisMediaControl
+End Class
---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,107 @@
+﻿Option Explicit On
+Imports System.Drawing.Color
+Imports System.Math
+Public Class Form1
+    Const Pi = 3.14
+    Dim x As Single
+    Dim y As Single
+    Dim x0 As Single
+    Dim y0 As Single
+    Dim v As Single
+    Dim v0 As Single
+    Dim vx0 As Single
+    Dim vy0 As Single
+    Dim vx As Single
+    Dim vy As Single
+    Dim alpha As Single
+    Dim alpha0 As Single
+    Dim ax As Single
+    Dim ay As Single
+    Dim t As Single
+    Dim dt As Single
+    Dim PixColor As Color
+    Dim Xmax As Single
+    Dim Ymax As Single
+    Dim Vmax As Single
+    Private Axis1 As Object
+
+    Private Sub InitAxis()
+        Axis1.Axis_Type = 3
+        Axis1.Pix_type = 1
+        Axis1.x_Base = Val(TextBox2.Text)
+        Axis1.y_Base = Val(TextBox3.Text)
+        Axis1.Pix_type = 3
+        Axis1.Pix_Size = 0.05
+        Axis1.x_Name = "X"
+        Axis1.y_Name = "Y"
+        Axis1.BackColor = White
+        Axis1.AxisDraw()
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        alpha0 = Val(TextBox5.Text)
+        v0 = Val(TextBox4.Text)
+        dt = Val(TextBox6.Text)
+        t = 0
+        v = v0
+        vx0 = v0 * Cos(alpha0 * Pi / 180)
+        vy0 = v0 * Sin(alpha0 * Pi / 180)
+        x0 = 0
+        y0 = Val(TextBox1.Text)
+        Vmax = v0
+        Xmax = x0
+        Ymax = y0
+        Timer1.Enabled = True
+    End Sub
+
+        Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        t = t + dt
+        If RadioButton1.Checked Then
+            ax = -v * vx
+            ay = -1 - v * vy
+            PixColor = Black
+        Else
+            ax = 0
+            ay = -1
+            PixColor = Red
+        End If
+        vx = vx0 + ax * dt
+        vy = vy0 + ay * dt
+        x = x0 + (vx0 + vx) * dt + ax * dt * dt / 2
+        y = y0 + (vy0 + vy) * dt + ay * dt * dt / 2
+
+        Axis1.Pix_Size = 0.005
+        Axis1.PixDraw(x, y, PixColor, 1)
+        Axis1.StatToDin()
+        'ядро
+        Axis1.Pix_Size = 0.02
+        Axis1.PixDraw(x, y, PixColor, 2)
+        Axis1.DinToPic()
+        If y < 0 Then
+            Timer1.Enabled = False
+            TextBox7.Text = Str(Round(Xmax, 2))
+            TextBox8.Text = Str(Round(Ymax, 2))
+            TextBox9.Text = Str(Round(Vmax, 2))
+        End If
+
+        x0 = x
+        y0 = y
+        vx0 = vx
+        vy0 = vy
+        v = Sqrt(vx ^ 2 + vy ^ 2)
+        If Abs(x) > Xmax Then Xmax = Abs(x)
+        If Abs(y) > Ymax Then Ymax = Abs(y)
+        If v > Vmax Then Vmax = v
+        TextBox10.Text = Str(Round(t, 2))
+    End Sub
+End Class
